dea0e832ca0518d65b025107158dbf241891d923
Zeng <yz720@ic.ac.uk>
2024-06-15 11:05:50 +0100

    Update bounds of rank sum

--- R/boundsSumRank.R
+++ R/boundsSumRank.R
@@ -17,34 +17,37 @@ boundsSumRank <- function(X, Y, ties, lower.boundary, upper.boundary){
   # Observed sample size
   n_prime <- length(X_prime)
   m_prime <- length(Y_prime)
-  
+
   # Sum of ranks of X_prime in Z_prime
   rankSumX_prime <- sum(r[1:n_prime])
 
   # Bounds of sum of ranks of X in Z without ties
   lowerBoundSumRank <- rankSumX_prime + (n - n_prime) * (n + n_prime + 1)/2
   upperBoundSumRank <- rankSumX_prime + (n * (n + 2*m + 1) - n_prime * (n_prime + 2*m_prime + 1))/2
-  
+
   # Bounds of sum of ranks of X in Z with ties
   if(ties) {
     # If ties are allowed, tighter bounds may exist
-    
-    if (lower.boundary == 'equal'){
-      a <- min(Z_prime)
+    min_observed <- min(Z_prime)
+    max_observed <- max(Z_prime)
+
+    if (lower.boundary == min_observed){
+      a <- min_observed
     }else{
       a <- -Inf
     }
-          
-    if (upper.boundary == 'equal'){
-      b <- max(Z_prime)
+
+    if (upper.boundary == max_observed){
+      b <- max_observed
     }else{
       b <- Inf
     }
-   
-    lowerBoundSumRank <- lowerBoundSumRank + (sum(Y_prime == a) * (n - n_prime) 
+
+    lowerBoundSumRank <- lowerBoundSumRank + (sum(Y_prime == a) * (n - n_prime)
                                            + sum(X_prime == b) * (m - m_prime))/2
-    upperBoundSumRank <- upperBoundSumRank - (sum(X_prime == a) * (m - m_prime) 
-                                           + sum(Y_prime == b) * (n - n_prime))/2   
+
+    upperBoundSumRank <- upperBoundSumRank - (sum(X_prime == a) * (m - m_prime)
+                                           + sum(Y_prime == b) * (n - n_prime))/2
   }
 
   return(c(lowerBoundSumRank, upperBoundSumRank))
