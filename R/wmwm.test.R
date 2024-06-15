e1e01609d7e1879ec6cef1f5a40c9db17daa9345
Zeng <yz720@ic.ac.uk>
2024-06-15 14:29:49 +0100

    modify the document of wmwm.test function

--- R/wmwm.test.R
+++ R/wmwm.test.R
@@ -1,48 +1,81 @@
-# This function performs the method proposed in the paper ``On two-sample testing for data with arbitrarily missing values'.
-# When no missing data is presented, this function returns exactly the same results as stats::wilcox.test().
-# When missing data is presented, this function returns a p-value that controls the Type I error regardless of the values of missing data, along with the bounds of the Wilcoxon-Mann-Whitney test statistic and the bounds of the p-value of the test without missing data.
+# This function performs the two-sample hypothesis method with missing data
+# proposed in the paper ``On two-sample testing for data with arbitrarily
+# missing values'.
+# When no missing data is presented, this function returns exactly
+# the same results as stats::wilcox.test().
+# When missing data is presented, this function returns a p-value that controls
+# the Type I error regardless of the values of missing data,
+# along with the bounds of the Wilcoxon-Mann-Whitney test statistic,
+# and the bounds of the p-value without missing data.
 
 #' @md
 #' @export
 #'
-#' @title Two-sample Wilcoxon-Mann-Whitney Test in the Presence of Arbitrarily Missing Data
+#' @title Two-sample Wilcoxon-Mann-Whitney Test in the Presence of
+#' Arbitrarily Missing Data
 #'
-#' @description Performs Wilcoxon-Mann-Whitney two-sample hypothesis tests on (potentially) partially observed vectors of data with controlled Type I error regardless of values of missing data.
+#' @description Performs Wilcoxon-Mann-Whitney two-sample hypothesis tests
+#' on (potentially) partially observed vectors of data
+#' with controlled Type I error regardless of values of missing data.
 #'
 #' @usage wmwm.test(X, Y, alternative = c("two.sided", "less", "greater"),
 #' ties = NULL, lower.boundary = -Inf,
 #' upper.boundary = Inf, exact = NULL, correct = TRUE)
 #'
-#' @param X,Y numeric vectors of data values with potential missing data. Inf and -Inf values will be omitted.
-#' @param alternative a character string specifying the alternative hypothesis, must be one of "two.sided" (default), "greater" or "less". You can specify just the initial letter.
-#' @param exact a logical indicating whether the bounds should be of an exact p-value.
+#' @param X,Y numeric vectors of data values with potential missing data.
+#' Inf and -Inf values will be omitted.
+#' @param alternative a character string specifying the alternative hypothesis,
+#' must be one of "two.sided" (default), "greater" or "less".
+#' You can specify just the initial letter.
+#' @param exact a logical indicating whether the bounds should be of an
+#' exact p-value.
 #' @param ties a logical indicating whether samples could be tied.
 #'   * If observed samples contain tied samples, ties defaults to TRUE.
 #'   * If observed samples do not contain tied samples, ties defaults to FALSE.
-#' @param lower.boundary (when ties is TRUE) a number specifying the lower bound of the data set, must be smaller or equal than the minimum of all observed data.
-#' @param upper.boundary (when ties is TRUE) a number specifying the upper bound of the data set, must be larger or equal than the maximum of all observed data.
-#' @param correct a logical indicating whether the bounds should be of a p-value applying continuity correction in the normal approximation.
+#' @param lower.boundary (when ties is TRUE) a number specifying the
+#' lower bound of the data set, must be smaller or equal than
+#' the minimum of all observed data.
+#' @param upper.boundary (when ties is TRUE) a number specifying the
+#' upper bound of the data set, must be larger or equal than
+#' the maximum of all observed data.
+#' @param correct a logical indicating whether the bounds should be of
+#' a p-value applying continuity correction in the normal approximation.
 #'
-#' @details  This function allows us to perform two-sample Wilcoxon-Mann-Whitney test in the presence of missing data with controlled controlled Type I error regardless of the values of missing data.
+#' @details  This function allows us to perform
+#'  Wilcoxon-Mann-Whitney test in the presence of missing data
+#'  with controlled controlled Type I error
+#'  regardless of the values of missing data.
 #'
-#' This function will compute the bounds of the Wilcoxon-Mann-Whitney test statistic and p-values without missing data. The p-value of this test is then returned as the maximum possible p-value of the Wilcoxon-Mann-Whitney test.
+#' This function will compute the bounds of the Wilcoxon-Mann-Whitney
+#' test statistic and its p-value without missing data.
+#' The p-value of this test is then returned as the
+#' maximum possible p-value of the Wilcoxon-Mann-Whitney test.
 #'
-#' When `X` and `Y` consist of real-valued, potentially tied samples, by additionally specifying `ties = TRUE` (this is automatically done if ties exist in observed samples `X'` and `Y'`),
+#' When `X` and `Y` consist of real-valued, potentially tied samples,
+#' by additionally specifying `ties = TRUE`
+#' (this is automatically done if ties exist in observed samples `X'` and `Y'`),
 #' this function deals with ties using mid-ranks method.
 #'
-#' By default (if exact is not specified), this function returns bounds of an exact p-value if the `X` and `Y` both contain less than 50 samples and there are no ties. Otherwise, bounds of a p-value calculated using normal approximation with continuity correction will be returned.
+#' By default (if exact is not specified), this function returns
+#' bounds of an exact p-value if the `X` and `Y` both contain less than
+#' 50 samples and there are no ties.
+#' Otherwise, bounds of a p-value calculated using normal approximation
+#' with continuity correction will be returned.
 #'
 #' @return
 #'
 #'  \item{p.value}{the p-value for the test}
 #'
-#'  \item{bounds.statistic}{bounds of the value of the Wilcoxon-Mann-Whitney test statistic without missing data}
+#'  \item{bounds.statistic}{bounds of the value of the Wilcoxon-Mann-Whitney
+#'  test statistic without missing data}
 #'
-#'  \item{bounds.pvalue}{bounds of the p-value of the Wilcoxon-Mann-Whitney test without missing data.}
+#'  \item{bounds.pvalue}{bounds of the p-value of the Wilcoxon-Mann-Whitney
+#'  test without missing data.}
 #'
 #'  \item{alternative}{a character string describing the alternative hypothesis.}
 #'
-#'  \item{ties.method}{a character string describing whether samples are considered tied.}
+#'  \item{ties.method}{a character string describing whether samples are
+#'  considered tied.}
 #'
 #'  \item{description.bounds}{a character string describing the bounds.}
 #'
@@ -51,10 +84,15 @@
 #'
 #' @references
 #' \itemize{
-#'  \item Zeng Y, Adams NM, Bodenham DA. On two-sample testing for data with arbitrarily missing values. arXiv preprint arXiv:2403.15327. 2024 Mar 22.
-#'  \item Mann, Henry B., and Donald R. Whitney. "On a test of whether one of two random variables is stochastically larger than the other." \emph{The annals of mathematical statistics} (1947): 50-60.
-#'  \item Lehmann, Erich Leo, and Howard J. D'Abrera. Nonparametrics: statistical methods based on ranks. Holden-day, 1975.
-#'  \item Schafer, Joseph L., and John W. Graham. "Missing data: our view of the state of the art." \emph{Psychological methods} 7.2 (2002): 147.
+#'  \item Zeng Y, Adams NM, Bodenham DA. On two-sample testing for data with
+#'  arbitrarily missing values. arXiv preprint arXiv:2403.15327. 2024 Mar 22.
+#'
+#'  \item Mann, Henry B., and Donald R. Whitney. "On a test of whether one
+#'  of two random variables is stochastically larger than the other."
+#'  \emph{The annals of mathematical statistics} (1947): 50-60.
+#'
+#'  \item Lehmann, Erich Leo, and Howard J. D'Abrera. Nonparametrics:
+#'  statistical methods based on ranks. Holden-day, 1975.
 #' }
 #'
 #' @seealso [stats::wilcox.test()]
