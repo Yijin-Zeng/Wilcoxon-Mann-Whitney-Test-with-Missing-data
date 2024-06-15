1b6b37f9936a23aa62cb620294521bf4662ee80f
Zeng <yz720@ic.ac.uk>
2024-06-15 12:01:55 +0100

    add unit test for bounds of WMW test statistics

--- tests/testthat/test-boundsSumRank.R
+++ tests/testthat/test-boundsSumRank.R
@@ -8,7 +8,6 @@ test_that("boundsSumRanks works without ties", {
 })
 
 test_that("boundsSumRanks works with ties", {
-
   expect_equal(boundsSumRank(X = c(1, 1, NA), Y = c(2.1, 3.5),
                              ties = TRUE, lower.boundary = 1,
                              upper.boundary = 3.5), c(6, 7.5))
@@ -16,5 +15,4 @@ test_that("boundsSumRanks works with ties", {
   expect_equal(boundsSumRank(X = c(1, 1, NA), Y = c(2.1, 3.5),
                              ties = TRUE, lower.boundary = 0,
                              upper.boundary = 4), c(6, 8))
-
 })
