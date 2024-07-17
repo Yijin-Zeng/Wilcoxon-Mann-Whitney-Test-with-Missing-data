57ef93c38a7e6a742ce9f1744226aa83032e0022
Zeng <yz720@ic.ac.uk>
2024-07-17 17:13:25 +0100

    Supress warnings in test-wmwm.test.R

--- tests/testthat/test-wmwm.test.R
+++ tests/testthat/test-wmwm.test.R
@@ -9,7 +9,7 @@ test_that("bounds of WMW test Statistic without ties", {
   expect_equal(RES$bounds.statistic[2], 20)
 })
 
-test_that("ties are not considered", {
+test_that("check ties are not considered", {
   X <- c(6.2, 3.5, NA, 7.6, 9.2)
   Y <- c(0.2, 1.3, -0.5, -1.7)
   RES <- wmwm.test(X,Y, ties = FALSE)
@@ -21,7 +21,7 @@ test_that("bounds of WMW test Statistic with ties
   X <- c(6, 9, NA, 7, 9)
   Y <- c(0, 1, 0, -1)
 
-  RES <- wmwm.test(X,Y, ties = TRUE)
+  RES <- suppressWarnings(wmwm.test(X,Y, ties = TRUE))
   # minimum possible WMW test statistic
   expect_equal(RES$bounds.statistic[1], 16)
   # maximum possible WMW test statistic
@@ -33,16 +33,16 @@ test_that("bounds of WMW test Statistic with ties
   X <- c(6, 9, NA, 7, 9)
   Y <- c(0, 1, 0, -1)
 
-  RES <- wmwm.test(X,Y, ties = TRUE, lower.boundary = -1, upper.boundary = 9)
+  RES <- suppressWarnings(wmwm.test(X,Y, ties = TRUE, lower.boundary = -1, upper.boundary = 9))
   # minimum possible WMW test statistic
   expect_equal(RES$bounds.statistic[1], 16.5)
   # maximum possible WMW test statistic
   expect_equal(RES$bounds.statistic[2], 20)
 })
 
-test_that("ties are considered", {
+test_that("check ties are considered", {
   X <- c(6, 9, NA, 7, 9)
   Y <- c(0, 1, 0, -1)
-  RES <- wmwm.test(X,Y, ties = TRUE)
+  RES <- suppressWarnings(wmwm.test(X,Y, ties = TRUE))
   expect_equal(RES$ties.method, TRUE)
 })
