bb5d955a014584b3e8ed0d2fc7787e92088514d9
Zeng <yz720@ic.ac.uk>
2024-07-17 17:19:20 +0100

    Supress warnings in test-checkTies

--- tests/testthat/test-checkTies.R
+++ tests/testthat/test-checkTies.R
@@ -16,10 +16,11 @@ test_that("expect warning when all samples are observed as unique numbers
           })
 
 
-test_that("expected warning when all samples are observed and
+test_that("expected warnings when all samples are observed and
           tied samples exist, but ties are specified to FALSE", {
             X <- c(1,1,2,5)
             Y <- c(2,3,5)
-            expect_warning(checkTies(X,Y,ties = FALSE),
-                           "all samples are observed with tied observations, ties can only be TRUE")
+            warnings <- capture_warnings(checkTies(X,Y,ties = FALSE))
+            expect_equal(warnings[1], "observed samples are tied, you may want to specify ties = TRUE")
+            expect_equal(warnings[2], "all samples are observed with tied observations, ties can only be TRUE")
           })
